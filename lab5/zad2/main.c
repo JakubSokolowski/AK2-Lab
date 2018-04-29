3ff3169f9ef8f97739c21d9fbbbdba4cf3266cb9
JakubSokolowski <jakub.sokolowski1996@gmai.com>
2018-04-29 21:46:37 +0200

    Recover (rewrite...) accidentaly changed inline assembly

--- lab5/zad2/main.c
+++ lab5/zad2/main.c
@@ -1,39 +1,37 @@
 #include <stdio.h>
 
-int number = 10000;
-char output[512];
-
-int gcd( int a, int b ) {
-    int result ;
-    /* Compute Greatest Common Divisor using Euclid's Algorithm */
-    __asm__ __volatile__ ("movl %1, %%eax;"
-                          "movl %2, %%ebx;"
-                          "CONTD: cmpl $0, %%ebx;"
-                          "je DONE;"
-                          "xorl %%edx, %%edx;"
-                          "idivl %%ebx;"
-                          "movl %%ebx, %%eax;"
-                          "movl %%edx, %%ebx;"
-                          "jmp CONTD;"
-                          "DONE: movl %%eax, %0;" : "=g" (result) : "g" (a), "g" (b)
-    );
-
-    return result ;
-}
+long number = 567;
+char output_arr[10];
+char rev_output_arr[10];
 
 int main() {
-    __asm__ __volatile__ ("movl %1, %%eax;"
-                          "movl %2, %%ebx;"
-                          "CONTD: cmpl $0, %%ebx;"
-                          "je DONE;"
-                          "xorl %%edx, %%edx;"
-                          "idivl %%ebx;"
-                          "movl %%ebx, %%eax;"
-                          "movl %%edx, %%ebx;"
-                          "jmp CONTD;"
-                          "DONE: movl %%eax, %0;" : "=g" (output) : "g" (number) 
+    __asm__ __volatile__ ("mov %0, %%rax;"
+                          "movq $0, %%rdi;"
+                          "movq $8, %%r10;"
+                          "division_loop: movl $0, %%edx;"
+                          "div %%r10;"              
+                          "add $0x30, %%edx;"
+                          "mov %%dl, (%1, %%rdi,1);"
+                          "inc %%rdi;" 
+                          "cmp %%rax, %%r10;"
+                          "jle division_loop;"
+                          "add $0x30, %%eax;"
+                          "mov %%al, (%1,%%rdi,1);"     
+                          "movq $0, %%rax;"
+                          "movq $0, %%rsi;"
+                          "reverse_loop: cmp $0, %%rdi;"
+                          "jl end;"
+                          "mov (%1, %%rdi,1), %%al;"
+                          "mov %%al, (%2, %%rsi, 1);"
+                          "inc %%rsi;"
+                          "dec %%rdi;"
+                          "jmp reverse_loop;"
+                          "end:;"
+                          : 
+                          : "r" (number), "r" (output_arr), "r" (rev_output_arr)
+                          : "%rax", "%rdx", "%rdi", "%rsi", "%r10"
     );
 
-    printf("Numer in base 8: %s", output);
+    printf("Numer in base 8: %s\n", rev_output_arr);
     return 0 ;
 }
