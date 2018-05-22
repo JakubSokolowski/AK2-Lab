c033567188cd99a9aaabd0f72e718e8a4ac139a4
JakubSokolowski <jakub.sokolowski1996@gmai.com>
2018-05-22 17:05:59 +0200

    Add files from lab 6

---
+++ lab6/zad1/main.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+extern int check_mode();
+extern int set_mode(int mode);
+
+int main(void) {
+  int option=1, mode=1;
+	do {
+	  printf("=== MENU: ===\n");
+	  printf("1 - check rounding mode\n");
+	  printf("2 - set rounding mode:\n");
+	  printf("0 - exit\n");
+	  scanf("%d", &option);
+
+	  switch(option) {
+			case 1:
+			    printf("\nCurrent rounding mode: ");
+			    switch(check_mode()) {
+			        case 0: printf("Round to nearest\n"); break;
+			        case 1: printf("Round down\n"); break;
+			        case 2: printf("Round up\n"); break;
+			        case 3: printf("Truncate\n"); break;
+			    }
+			    break;
+			case 2:
+			    printf("\n0 - Round to nearest\n");
+			    printf("1 - Round down\n");
+			    printf("2 - Round up\n");
+			    printf("3 - Truncate\n");
+			    scanf("%d", &mode);
+			    if (mode > 3 || mode < 0) 
+						printf("Invalid value");
+			    else 
+						set_mode(mode);
+			    printf("\n");
+			    break;
+	  }
+	} while(option!=0);
+	return 0;
+}
---
+++ lab6/zad1/mode.s
@@ -0,0 +1,53 @@
+.section .data
+	control_word: .short 0
+.section .text
+
+.global check_mode, set_mode
+.type check_mode, @function
+.type set_mode, @function
+
+check_mode:
+	push    %rbp
+	mov     %rsp, %rbp
+
+	mov     $0, %rax
+	fstcw   control_word
+  fwait
+	mov     control_word, %ax
+	# Wydobycie bitów trybu zaokrąglania - 10,11
+	and     $0xC00, %ax # 0000 1100 0000 0000
+	shr     $10, %ax
+  
+  # 00 - Nearest, 01 - Down, 10 - Up, 11 - truncate
+
+	mov     %rbp, %rsp
+	pop     %rbp
+	ret
+
+set_mode:
+	push        %rbp
+	mov         %rsp, %rbp
+
+	mov         $0, %rax
+	fstcw       control_word
+	fwait
+	mov         control_word, %ax
+
+	# Wyzerowanie zaokrąglania 10, 11
+	# (00 - początkowo nearest)
+	and         $0xF3FF, %ax # 1111 0011 1111 1111
+
+	# Przesunięcie %rdi, tak aby "wpasował" się w
+  # miejsce z zerami
+	shl         $10, %rdi
+	
+	# Sklejenie rejestrów, wynik w %ax
+	xor         %di, %ax
+
+	# Zapis zmienionej wartości do rejestru CW
+	mov         %ax, control_word
+	fldcw       control_word
+
+	mov         %rbp, %rsp
+	pop         %rbp
+	ret
