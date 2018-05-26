c390d41498165ae088b2775e3b24a2ec0abd2d20
JakubSokolowski <jakub.sokolowski1996@gmai.com>
2018-05-26 16:39:56 +0200

    Add function to compute tylor series expansion for exponential function

--- lab6/zad2/exp.s
+++ lab6/zad2/exp.s
@@ -1,4 +1,6 @@
 .section .data
+    base: .double 1.0
+    num: .double 0.0
 .section .text
 .global exp_approx
 .global factorial
@@ -7,14 +9,25 @@
 exp_approx:
     # %rdi - number of steps
     # %xmmo - value of exponent
-
     # 1 + x + x^2/2! + x^3/3!
     push %rbp
     mov %rsp, %rbp
-    
-    # 
-
-    leave
+    movsd base, %xmm2
+    movsd %xmm0, num
+    approx_loop:
+        cmp $0, %rdi
+        je end_approx
+        call factorial       # Compute factorial for n, result in %rax
+        movsd num, %xmm0   # Compute x ^ n, result in %xmm0
+        call power
+        cvtsi2sd %rax, %xmm1 # Convert the factorial into double, for division
+        divsd %xmm1, %xmm0   # Divide the x ^ n by factorial, store in %xmm2
+        addsd %xmm0, %xmm2
+        dec %rdi
+        jmp approx_loop
+    end_approx:
+        movsd %xmm2, %xmm0
+        leave
 ret
 # Compute the factorial of number in %rdi and place result in %rax
 .type factorial, @function
@@ -42,6 +55,7 @@ power:
     # %xmm0 - number
     push %rbp
     mov %rsp, %rbp
+    push %rdi
     movsd %xmm0, %xmm1
     power_loop:
         cmp $1, %rdi
@@ -50,6 +64,7 @@ power:
         mulsd %xmm1, %xmm0
         jmp power_loop
     end_power:
+    pop %rdi
     leave
 ret
     
